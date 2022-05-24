bdd551052bbbd29eb405ff7334e212bfd3d4f5b5
Ilie <kid@ml1.net>
2022-05-24 18:21:16 +0200

    add css file in public dir; integrate in server.tsx

--- app/src/server.tsx
+++ app/src/server.tsx
@@ -7,6 +7,7 @@ export default function Root() {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="style.css" />
         <HydrationScript />
       </head>
       <body>
