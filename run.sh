f57a16fdbbef91e9b2e0c5d6fbed6fe52663902d
Mircea Ilie Ploscaru <kid@ml1.net>
2022-05-24 18:23:59 +0200

    update run.sh w/ new line

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-deno run --no-check --allow-env --allow-net --allow-read --allow-run --allow-write --import-map ./bundler/import_map.json ./bundler/index.ts
+deno run --no-check -A --import-map bundler/import_map.json bundler/index.ts
